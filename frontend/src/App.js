4de1061f3ee8814bf6d05af1e0e679926ea7af99
Carlos Zahid <zahidhdez@outlook.com>
2023-11-25 15:39:22 -0600

    pruebas y tal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import EditCertificate from './EditCertificate'
 import { Routes, Route} from 'react-router-dom';
 import Layout from './Layout';
 import RequireAuth from './RequireAuth'
+import Unauthorized from './Unauthorized'
 
 
 function App(){
@@ -19,9 +20,11 @@ function App(){
         <Route path='/' element={<Layout/>}> 
         
           <Route path='login' element={<Login/>}/> 
+          <Route path='unauthorized' element={<Unauthorized/>}/> 
 
           {/*rutas protegidas*/}
           <Route element={<RequireAuth allowedRoles={["Admin"]}/>}>
+            <Route path='/' element={<Home/>}/> 
             <Route path='home' element={<Home/>}/> 
             <Route path='students' element={<Students/>}/> 
             <Route path='students/details/:id' element={<StudentsDetails/>}/> 
